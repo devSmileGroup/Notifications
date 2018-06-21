7ba4e9bda52600161a175fd928a85d34c4cd1793
kolya-mazurok2 <kolya.mazurok2@gmail.com>
2018-06-21 13:37:36 +0300

    upgrade query findByEmailStatus

--- src/main/java/com/dev/booking/repositories/NotificationRepository.java
+++ src/main/java/com/dev/booking/repositories/NotificationRepository.java
@@ -1,14 +1,16 @@
 package com.dev.booking.repositories;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import com.dev.booking.models.Notification;
 
 public interface NotificationRepository extends MongoRepository<Notification, ObjectId> {
-	@Query("{'$or': [{'email_info.status' : ?0 }, {'email_info.status' : ?1 }]}")
-	public List<Notification> findByEmailStatus(String value0, String value1);
+	@Query("{'$and':[{'$or':[{'email_info.status':?0 },{'email_info.status':?1 }]},{'modified_date':{$lt:?2}}]}")
+	public List<Notification> findByEmailStatus(String status1, String status2, LocalDateTime date, Pageable pageable);
 }
